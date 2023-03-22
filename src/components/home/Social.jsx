7fb1b9e11121a4138cd178b92e134eef8361c1c3
Hang Ngo <thngo@ualberta.ca>
2023-03-22 12:20:58 -0600

    new react porfolio

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="" className="home__social-icon" target="-blank">
+            <i class="uil uil-instagram"></i>
+        </a>
+        <a href="https://github.com/thihangng" className="home__social-icon" target="-blank">
+            <i class="uil uil-github-alt"></i>
+        </a>
+    
+    </div>
+  )
+}
+
+export default Social
